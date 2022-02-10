eabbe041fdb6b1f3b3374b55ac55f17e89c55c9f
Keshavi <kishavi69@gmail.com>
2022-02-09 20:21:25 -0600

    YESSSSSSSSSSSSSSSSS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 
+int main(){
 
-main(){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    
-    
-    
     return 0;
 }
+	
